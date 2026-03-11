b651bc14044ba1e3adffb00e567688c4ac9937d2
vaibhavwarade815 <varadevaibhav815@gmail.com>
2026-03-11 17:32:44 +0530

    search section js provided

---
+++ js/home.js
@@ -0,0 +1,133 @@
+/* JOB DATA */
+
+let jobs= [
+
+{
+title:"Frontend Developer Intern",
+company:"Google",
+location:"Pune",
+type:"Internship",
+salary:"15000/month"
+},
+
+{
+title:"Frontend Developer",
+company:"Amazon",
+location:"Mumbai",
+type:"Full Time",
+salary:"40000/month"
+},
+
+{
+title:"Frontend Developer",
+company:"Flipkart",
+location:"Bangalore",
+type:"Full Time",
+salary:"45000/month"
+},
+
+{
+title:"Frontend Developer",
+company:"Wipro",
+location:"Hyderabad",
+type:"Internship",
+salary:"18000/month"
+},
+
+{
+title:"Backend Developer",
+company:"Microsoft",
+location:"Mumbai",
+type:"Full Time",
+salary:"60000/month"
+},
+
+{
+title:"Backend Developer",
+company:"IBM",
+location:"Pune",
+type:"Full Time",
+salary:"55000/month"
+},
+
+{
+title:"Backend Developer",
+company:"Oracle",
+location:"Bangalore",
+type:"Full Time",
+salary:"52000/month"
+},
+
+{
+title:"Backend Developer",
+company:"Accenture",
+location:"Hyderabad",
+type:"Full Time",
+salary:"50000/month"
+},
+
+{
+title:"Web Designer",
+company:"Infosys",
+location:"Bangalore",
+type:"Internship",
+salary:"12000/month"
+},
+
+{
+title:"Web Designer",
+company:"TCS",
+location:"Mumbai",
+type:"Full Time",
+salary:"35000/month"
+},
+
+{
+title:"Web Designer",
+company:"Capgemini",
+location:"Pune",
+type:"Full Time",
+salary:"30000/month"
+},
+
+{
+title:"Web Designer",
+company:"HCL",
+location:"Hyderabad",
+type:"Internship",
+salary:"15000/month"
+},
+
+{
+title:"Java Developer",
+company:"TCS",
+location:"Hyderabad",
+type:"Full Time",
+salary:"50000/month"
+},
+
+{
+title:"Java Developer",
+company:"Cognizant",
+location:"Pune",
+type:"Full Time",
+salary:"48000/month"
+},
+
+{
+title:"Java Developer",
+company:"Dell",
+location:"Bangalore",
+type:"Full Time",
+salary:"52000/month"
+},
+
+{
+title:"Java Developer",
+company:"Zoho",
+location:"Mumbai",
+type:"Full Time",
+salary:"47000/month"
+}
+
+]
